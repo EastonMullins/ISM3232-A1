55ac8af40f1b5faef8fd63bc12a7796c5b38facf
Easton <eastonmullins@usf.edu>
2025-01-26 12:42:42 -0500

    Task 1 - Employee Information

---
+++ Challenge 1/cc_1.js
@@ -0,0 +1,8 @@
+let employeeName = "Easton Mullins";
+const employeeID = 456;"
+var isActive = true;
+
+console.log("Employee Name:" ,employeeName, "Type:" , typeof employeeName);
+console.log("ID:" , employeeID, "Type:" , typeof employeeID);
+console.log("Status:", isActive, "Type:", typeof isActive);
+//Task 1 Employee Information
