1084308f52f58ee00db5f28edbe87698951b623a
Easton <eastonmullins@usf.edu>
2025-01-26 14:07:50 -0500

    Task 1 - Store Inventory

---
+++ Challenge 2/cc_2.js
@@ -0,0 +1,7 @@
+let products = ["Cheeseburger", "Soda", "Chicken Fingers", "French Fries", "Milkshake"];
+products.push ("Mac and Cheese");
+console.log(products);
+let productsUpdated = products.pop();
+console.log(productsUpdated);
+console.log(products)
+// Task 1 Store Inventoty
