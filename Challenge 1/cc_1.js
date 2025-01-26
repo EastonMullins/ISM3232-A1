7c60ec9d7c57ce4cad4cb6958c67230a45656cbc
Easton <eastonmullins@usf.edu>
2025-01-26 12:53:50 -0500

    Task 2 - Product Details

--- Challenge 1/cc_1.js
+++ Challenge 1/cc_1.js
@@ -1,8 +1,18 @@
 let employeeName = "Easton Mullins";
-const employeeID = 456;"
+const employeeID = 456;
 var isActive = true;
 
 console.log("Employee Name:" ,employeeName, "Type:" , typeof employeeName);
 console.log("ID:" , employeeID, "Type:" , typeof employeeID);
 console.log("Status:", isActive, "Type:", typeof isActive);
 //Task 1 Employee Information
+
+
+let productName = "Cheeseburger";
+const productPrice = 4.99;
+var isAvailable = true;
+
+console.log("Product Name:" ,productNameName, "Type:" , typeof productName);
+console.log("Price:" , productPrice, "Type:" , typeof productPrice);
+console.log("In Stock:", isAvailable, "Type:", typeof isAvailable);
+//Task 2 Product Details
