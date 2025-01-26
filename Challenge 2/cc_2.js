1c1ff95f613f0fc22bb07e5142c654eb5894b3a1
Easton <eastonmullins@usf.edu>
2025-01-26 14:40:48 -0500

    Task 2 - Student Scores

--- Challenge 2/cc_2.js
+++ Challenge 2/cc_2.js
@@ -4,4 +4,15 @@ console.log(products);
 let productsUpdated = products.pop();
 console.log(productsUpdated);
 console.log(products)
-// Task 1 Store Inventoty
+// Task 1 Store Inventory
+
+let scores = [98, 76, 83, 91, 68];
+console.log("Original Scores: ",scores);
+scores.splice(1,1, 88);
+console.log("Updated Scores: ",scores);
+
+let total = scores.reduce((sum, score) => sum + score, 0);
+let average =total / scores.length;
+
+console.log("Average Score: ",average);
+//Task 2 Student Scores
